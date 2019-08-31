bbd097eb73cd1c890546704bfacd0bfed2e3e038
Josue Cespedes <josuecss@yahoo.com>
2019-08-31 18:54:26 -0400

    Initial commit

---
+++ server/config/express.js
@@ -0,0 +1,18 @@
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var swig = require ('swig');
+var express = require('express');
+
+
+module.exports = function(app, config){
+    app.engine('html',swig.renderFile);
+    app.set('view engine', 'html');
+    app.set('views',config.rootPath + 'server/views');
+
+    app.set('view cache', false);
+    swig.setDefaults({cache:false});
+
+    app.use(logger('dev'));
+    app.use(bodyParser());
+    app.use(express.static(config.rootPath+'/public'))
+};
