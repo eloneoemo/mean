004b07ab998c4157e894764427fe6f671e773984
Josue Cespedes <josuecss@yahoo.com>
2019-09-06 11:57:27 -0400

    corregido

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-//Toda la creación y configuración del servidor de Node.js
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app);
 
-//Se agregó
 var config = {
-	rootPath :__dirname
+	rootPath : __dirname
 };
+
 require('./server/config/express')(app, config);
 require('./server/config/routes')(app);
 
